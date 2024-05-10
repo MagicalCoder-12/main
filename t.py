576b60cf757946da6cec884b03de91546f3881c6
MagicalCoder-12 <kuppamajith@gmail.com>
2024-05-10 13:09:35 +0530

    renamed:    Assets/Items/Fruits/banana.png -> Assets/Items/Fruits/Bananas.png 	modified:   t.py

--- t.py
+++ t.py
@@ -255,7 +255,7 @@ current_dir = os.path.dirname(__file__)
 # Create instances of PowerUp class
 powerup_images = [
     os.path.join(current_dir,'Assets','Items','Fruits','Apple.png'),
-    os.path.join(current_dir,'Assets','Items','Fruits','banana.png'),
+    os.path.join(current_dir,'Assets','Items','Fruits','Bananas.png'),
     os.path.join(current_dir,'Assets','Items','Fruits','Strawberry.png')
 ]
 
@@ -405,7 +405,7 @@ def main():
                         player.health += 10 
                     else:
                         player.health += 20  # Double the health restoration if under double bonus effect
-                elif powerup.type == "banana":
+                elif powerup.type == "Bananas":
                     double_bonus = pygame.time.get_ticks() + 10000  # Set the end time for doubling score
                 elif powerup.type == "Strawberry":
 
@@ -420,7 +420,7 @@ def main():
             text = font.render(f'Score: {score}', True, (0, 0, 0))
 
         screen.blit(text, (10, 10))
-         # Display timer counter for banana power-up
+         # Display timer counter for Bananas power-up
         if pygame.time.get_ticks() <= double_bonus:
             remaining_time = (double_bonus - pygame.time.get_ticks()) // 1000  # Convert milliseconds to seconds
             timer_text = font.render(f'Double Bonus: {remaining_time}s', True, (0, 0, 0))
@@ -447,9 +447,9 @@ def main():
                 
         # Define probabilities for each power-up type
         powerup_probabilities = {
-            "Apple": 0.2,  # 20% chance
-            "Banana": 0.1,  # 10% chance
-            "Strawberry": 0.05  # 5% chance
+            "Apple": 0.5,  # 20% chance
+            "Bananas": 0.7,  # 10% chance
+            "Strawberry": 0.1  # 5% chance
         }
 
       # Check collisions between lasers and asteroids
@@ -457,16 +457,12 @@ def main():
         for laser in player.lasers:
             for asteroid in asteroids:
                 if check_collision_lasers_asteroid([laser], asteroid['x'], asteroid['y']):
-                    print("Collision between laser and asteroid detected")
                     # Determine if a power-up should appear based on probabilities
                     for powerup_type, probability in powerup_probabilities.items():
                         if random.random() <= probability:
-                            print(f"Random Number: {random.random()}")
-                            print(f"Power-up Type: {powerup_type}, Probability: {probability}")
                             # Create a power-up at the asteroid's position
                             new_powerup_pos = [asteroid['x'], asteroid['y']]
-                            random_powerup_image = os.path.join("Assets", "Items", "Fruits", f"{powerup_type.lower()}.png")
-                            print(f"Selected Power-up Type: {powerup_type}")
+                            random_powerup_image = os.path.join("Assets", "Items", "Fruits", f"{powerup_type.capitalize()}.png")
                             new_powerup = PowerUp(random_powerup_image, 17, new_powerup_pos, scale_factor=2)
                             powerups.append(new_powerup)
                     lasers_to_remove.append(laser)  # Add the laser to the removal list
